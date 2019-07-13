8f6f68e522ab2901411a62cc8201a000751d307c
Marco <marco10mendieta@gmail.com>
2019-07-13 03:48:22 -0600

    Ingresa el nuevo programa
    Evalua el nuevo programa

---
+++ Z5ad6/src/z5ad6/Z5ad6.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package z5ad6;
+
+import z5ac4.toolMD5;
+
+/**
+ *
+ * @author MDLORIAL
+ */
+public class Z5ad6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String encmd5 = toolMD5.md5("Hola");
+        System.out.println("Encriptacion en MD5: " + encmd5);
+        
+    }
+    
+}
